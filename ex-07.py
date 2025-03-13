7b2e28019eb51881145da8e60b9d53ae1c0baa0c
diegofranco-ia <diego.franco1800@gmail.com>
2025-03-13 20:23:50 -0300

    Create ex-07.py

---
+++ ex-07.py
@@ -0,0 +1,13 @@
+#7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.Enriqueça o desenho com outros caracteres, simulando enfeites.
+print ("""       X
+      XXX
+     XXXXX
+    XXXXXXX
+   XXXXXXXXX
+  XXXXXXXXXXX
+ XXXXXXXXXXXXX
+XXXXXXXXXXXXXXX
+       XX
+       XX
+      XXXX
+""")
